c06e5ba711116ba4ced5ef7caa67f9c3173dd517
unknown <Alexander>
2025-05-16 00:15:27 -0400

    Pequeños cambios2

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.api_router import api_router
+from backend.app.api.api_router import api_router
 
 app = FastAPI()
 
